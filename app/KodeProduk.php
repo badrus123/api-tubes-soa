d151ed294505542457a2e0a5198a2525621579ba
Badrus shoolehk <badrus@student.telkomuniversity.ac.id>
2020-05-06 10:37:36 +0700

    bug redux

--- app/KodeProduk.php
+++ app/KodeProduk.php
@@ -11,11 +11,11 @@ Class KodeProduk extends Model
     public function dataKode($id,$kode){
         return $this->join('detail_produk', 'kode_produk.id_detail', '=', 'detail_produk.id_detail')
         ->join('produk', 'detail_produk.id_produk', '=', 'produk.id_produk')
-        ->select('*')->where('kode_produk.id_kode', '=', $id)
+        ->select('*')->where('kode_produk.id_detail', '=', $id)
         ->where('kode_produk.kode_produk', '=', $kode)->get();
     }
     public function searchKode($id,$kode){
-        return $this->where('kode_produk.id_kode', '=', $id)->where('kode_produk.kode_produk', '=', $kode)->first();
+        return $this->where('kode_produk.id_detail', '=', $id)->where('kode_produk.kode_produk', '=', $kode)->first();
     }
     protected $fillable = [
         'id_kode'
